17d1727383275324c32cb4593286c92ebf0adf79
YooLee <yookyunglee17@gmail.com>
2022-09-28 00:48:28 -0700

    [update] hash map > switch case문으로 수정. 마이너스 처리 되는 값을 4*num<ans 조건식으로 어떻게 생각해낸건지 확인 필요

--- romanToInt_yoolee.java
+++ romanToInt_yoolee.java
@@ -3,8 +3,8 @@ import java.util.Map;
 class Solution {
     public int romanToInt(String s) {
 
-        // 로마 문자에 해당하는 값을 해시맵 통해 매칭
-        Map<Character, Integer> map = new HashMap<>();
+        // 1. 로마 문자에 해당하는 값을 해시맵 통해 매칭
+      /*   Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
         map.put('X', 10);
@@ -12,7 +12,9 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
+
         int result = 0;
+        
         for(int i=0; i<s.length(); i++){
             if(i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i-1))){
             }
@@ -21,10 +23,42 @@ class Solution {
             }
             
         }
-
-        return result;
-    
+        return result; */
         // 흠.. 그런데 이 코드는 "MCMXCIV" input했을 때 마이너스 값 처리가 안되는 듯.. 해당 부분 처리 필요
+
+
+        // 2. 그냥 간단하게 switch case문으로 시도
+        int ans = 0, num = 0;
+
+        for (int i = s.length()-1; i >= 0; i--) {
+
+            switch(s.charAt(i)) {
+
+                case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+
+            }
+
+            // 여기 4*num이라는 조건을 어떻게 생각해낸거여.. 
+            if (4 * num < ans) ans -= num;
+            else ans += num; // 여기까지 수행한게 1턴
+            // "MCMXCIV" 입력 시 
+            // V: 5 break, 20 < 0 아니니까 ans = 5
+            // I: 1 break, 4 < 5 맞으니까 ans = 5-1 = 4
+            // C: 100 break, 400 < 4 아니니까 ans = 4+100 = 104
+            // X: 10 break, 40 < 104 맞으니까 ans = 104-10 = 94
+            // M: 1000 break, 4000 < 94 아니니까 ans = 94+1000 = 1094
+            // C: 100 break, 400 < 1094 맞으니까 ans = 1094-100 = 994
+            // M: 1000 break, 4000 < 994 아니니까 ans = 994+1000 = 1994 
+
+        }
+        
+        return ans; //MCMXCIV 입력 시 1994 리턴
 
 }
 }
